1d3c314bb17723bf643837137341813a5fe123f4
Sarah-Rose Prince <sarah-roseprince@Sarah-Roses-MacBook-Pro.local>
2023-03-29 20:45:03 -0500

    email counter

--- counter.py
+++ counter.py
@@ -3,6 +3,21 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+
+    emails = {}
+
+    for line in handle:
+        if line.startswith('From '):
+            line_words = line.split()
+            email_address = line_words[1]
+            
+            if email_address not in emails:
+                emails[email_address] = 1
+            else:
+                emails[email_address] += 1
+
+    max_key = max(emails, key=emails.get)
+    print(max_key, emails[max_key])
         
 
 ## if you want to test locally run > python counter.py
